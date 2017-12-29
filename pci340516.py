2162f586c82605a29d79f9216b1352b6c72a254b
逆井啓佑 <>
2017-12-30 00:10:58 +0900

    ver 0.0.1

--- pci340516.py
+++ pci340516.py
@@ -16,6 +16,10 @@ class InvalidCurrentError(Exception):
     pass
 
 
+class InvalidSamplingModeError(Exception):
+    pass
+
+
 class pci340516_driver(core.interface_driver):
     bit_flags_in = (
         (
@@ -102,11 +106,11 @@ class pci340516_driver(core.interface_driver):
         return bid
 
 
-    def _verify_current(self, current=0):
-        cur_limit = 100
-        if 0 < current <= cur_limit: pass
+    def _verify_current(self, current=0.0):
+        cur_limit = 100.0
+        if 0 <= current <= cur_limit: pass
         else:
-            msg = 'Current must be in 0[mA] - {0}[mA].'.format(cur_limit)
+            msg = 'Current must be in 0[mA] - {0}[mA] '.format(cur_limit)
             msg += 'while {0}[mA] is given.'.format(current)
             raise InvalidCurrentError(msg)
         return current
@@ -117,35 +121,36 @@ class pci340516_driver(core.interface_driver):
         ch_lim_final = 8
         
         if ch.find('-') == -1:
-            msg = 'Ch type must be chx-chy absolutelly'
+            msg = 'Ch type must be chx-chy absolutelly '
             msg += 'while {0} is given.'.format(ch)
             raise InvalidChtypeError(msg)
         
         ch_ = ch.split('-')
         if len(ch_) == 2: pass
         else :
-            msg = 'Ch type must be chx-chy absolutelly'
+            msg = 'Ch type must be chx-chy absolutelly '
             msg += 'while {0} is given.'.format(ch)
             raise InvalidChtypeError(msg)
             
         ch_initial, ch_final = int(ch_[0].replace('ch', '')), int(ch_[1].replace('ch', ''))
         if 1 <= ch_initial < ch_final <= 8: pass
         else:
-            msg = 'Ch range is in ch{0} - ch{1}'.format(ch_lim_initial, ch_lim_final)
-            msg = 'while ch{0} - ch{1} is given'.format(ch_initial, ch_final)
+            msg = 'Ch range is in ch{0} - ch{1} '.format(ch_lim_initial, ch_lim_final)
+            msg = 'while ch{0} - ch{1} is given.'.format(ch_initial, ch_final)
         ch = [i for i in range(ch_initial, ch_final+1)]
 
         return ch
 
 
-    def _current2list(self, current=0):
-        cur_range = 100
-        res = 16
+    def _current2list(self, current=0.0): # DA_1_100mV only.
+        current = float(current)
+        cur_range = 100.0
+        res = 16.0
         res_int = 2**res
 
-        if current == 100: bytes_c = res_int - 1
-        else:
-            bytes_c = int((current + cur_range)/(cur_range/(res_int/2)))
+        if current == 100.0: bytes_c = int(res_int - 1)
+        else: bytes_c = int((current + cur_range)/(cur_range/(res_int/2)))
+        bytes_c = int((current + cur_range)/(cur_range/(res_int/2)))
         bit_ = bin(bytes_c).replace('0b', '0'*(16-(len(bin(bytes_c))-2)))
         bit_list = [int(bit_[i]) for i in range(len(bit_))]
         bit_list.reverse()
@@ -159,8 +164,8 @@ class pci340516_driver(core.interface_driver):
 
         if mode == 'all_cout_disable': mode = ''
         elif mode == 'all_cout': mode = 'MD0'
-        elif mode == 'all_cclear': mode = 'MD1'
-        elif mode == 'all_cout_enale': mode = 'MD0 MD1'
+        elif mode == 'all_cout_clear': mode = 'MD1'
+        elif mode == 'all_cout_enable': mode = 'MD0 MD1'
 
         flags = mode
 
@@ -192,7 +197,7 @@ class pci340516_driver(core.interface_driver):
         return
 
     
-    def _da_output(self, ch='', current=0):
+    def _da_output(self, ch='', current=0.0):
         bar = 0
         size_ch = 1
         size_cur = 2
@@ -201,11 +206,11 @@ class pci340516_driver(core.interface_driver):
 
         data_ch = self._ch2bit(ch=ch)
         new_d_ch = core.list2bytes(data_ch)
-        self.write(bar_ch, offset_ch, new_d_ch)
+        self.write(bar, offset_ch, new_d_ch)
 
         data_cur = self._current2list(current=current)
         new_d_cur = core.list2byte(data_cur)
-        self.write(bar_ch, offset_cur, new_d_cur)
+        self.write(bar, offset_cur, new_d_cur)
         return
 
     
@@ -213,14 +218,13 @@ class pci340516_driver(core.interface_driver):
         self._set_sampling_config(mode='all_cout')
 
 
-    def da_output(self, ch='', current=0):
+    def output_da(self, ch='', current=0.0):
         bar = 0
         size = 2
         offset = 0x00
 
         ch = self._verify_ch(ch=ch)
-        current_ = self._verify_current(current)
-        current = self._current2list(current_)
+        current = self._verify_current(current=current)
         self._set_sampling_config(mode='all_cout_enable')
         self._da_onoff(onoff=1)
         time.sleep(0.01)
@@ -229,10 +233,74 @@ class pci340516_driver(core.interface_driver):
             self._da_output('ch{0}'.format(i), current)
 
         self._start_sampling()
-        # self._da_onoff(onoff=0)
+        time.sleep(0.01)
+        return
+
+
+    def set_sampling_range(self, mode='DA_0_100mA'):
+        bar = 0
+        size = 1
+        offset = 0x06
+
+        if mode == 'DA_0_1mA': mode = 'RG0'
+        elif mode == 'DA_0_100mA': mode = 'RG1'
+        else:
+            msg = 'Sampling range is DA_0_1mA of DA_0_100mA '
+            msg += 'while {0} is given.'.format(mode)
+            raise InvalidSamplingRangeError(msg)
+
+        flags = mode
+
+        self.set_flag(bar, offset, flags)
+        return
+
+    
+    def get_sampling_range(self):
+        bar = 0
+        size = 1
+        offset = 0x06
+
+        return self.read(bar, offset, size).print()
+        
+
+    def _verify_ch_sim(self, ch=''):
+        ch_lim_initial = 1
+        ch_lim_final = 8
+
+        if ch in ['ch{}'.format(i) for i in range(ch_lim_initial,ch_lim_final+1)]:
+            pass
+        else:
+            msg = 'Ch range is in ch{0} - ch{1} '.format(ch_lim_initial, ch_lim_final)
+            msg += 'while {0} is given.'.format(ch)
+            raise InvalidChRangeError(msg)
+            
+        return ch
+
+    
+    def output_da_sim(self, ch='', current=0.0):
+        bar = 0
+        size = 2
+        offset = 0x00
+
+        ch_ = self._verify_ch_sim(ch=ch)
+        current = self._verify_current(current=current)
+        self._set_sampling_config(mode='all_cout_enable')
+        self._da_onoff(onoff=1)
+        time.sleep(0.01)
+
+        self._da_output(ch_, current)
+
+        self._start_sampling()
         time.sleep(0.01)
         return
 
 
     def initialize(self):
+        self._da_onoff(onoff=1)
         self._set_sampling_config('all_cout_clear')
+
+
+    def finalize(self):
+        self._set_sampling_config('all_cout_clear')
+        self._da_onoff(onoff=0)
+
